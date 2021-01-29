4e61954acb9dad5cab478cc4201ed8faddf01f5f
Karim <karim.alaael-din@gmx.de>
2021-01-29 19:51:33 +0100

    fix: x capitalisation

--- delay_est.py
+++ delay_est.py
@@ -13,7 +13,7 @@ x_tr, x_te, y_tr, y_te, x_ref, y_ref = prep_delay_data(home)
 
 ann = ANN(shape=[50, 50, 20, 20], epochs=20000, verbose=2)
 
-ann.fit(X_tr, y_tr)
-score = ann.score(X_te, y_te)
+ann.fit(x_tr, y_tr)
+score = ann.score(x_te, y_te)
 
 print(score)
