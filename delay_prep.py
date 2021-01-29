67e12ce88fca849fc45899bcfa4d194a0cd116be
Karim <karim.alaael-din@gmx.de>
2021-01-29 15:07:33 +0100

    feat: refactor from previous project, initial

---
+++ delay_prep.py
@@ -0,0 +1,51 @@
+import numpy as np
+import scipy.stats as sps
+import pandas as pd
+
+from prep_utils import train_test_norm
+
+
+def prep_delay_data(split=0.15):
+    # Load double pulse data
+    double_inp = pd.read_table("./Data/double_inputs.tsv.gz")
+    double_out = pd.read_table("./Data/double_outputs.tsv.gz")
+
+    # select only delay columns
+    delay_out = double_out.loc[:, ["Delays", "DelayMask"]].copy()
+
+    # get prepared mask
+    delay_mask = delay_out["DelayMask"].values
+    delays_nan = delay_out["Delays"].isna().values
+    delay_mask = delay_mask & (not delays_nan)  # Also mask NaN values
+    delay_argmask = np.argwhere(delay_mask).flatten()  # create arg_mask to apply to inps and outputs
+
+    # apply masking of events
+    delay_inp = double_inp.iloc[delay_argmask].copy()
+    delay_out = delay_out.loc[delay_argmask, "Delays"]  # only select delay copy
+
+    # Filter input features by variance
+    feat_columns = [c for c in delay_inp if len(np.unique(delay_inp[c])) > 10]
+    delay_inp = delay_inp[feat_columns]
+
+    # Get mean absolute deviation of outputs
+    mad_delays = abs((delay_out.values
+                      - np.median(delay_out.values)) / sps.median_abs_deviation(delay_out.values))
+
+    # create mad and beam energy mask from relevant arrays
+    mad_mask = mad_delays < 4
+    emask = (delay_inp["f_63_ENRC"].values > 0.005) & (delay_inp["f_64_ENRC"].values > 0.005)
+
+    arg_mask = np.argwhere(mad_mask & emask).flatten()  # generate yet another arg_mask
+
+    # Apply arg_mask
+    delay_inp = delay_inp.iloc[arg_mask]
+    delay_out = delay_out.iloc[arg_mask]
+
+    # Reuse training_test split and normalisation across inputs
+    return train_test_norm(delay_inp, delay_out, split)
+
+
+if __name__ == "__main__":
+    # for debugging purposes only
+    x_tr, x_te, y_tr, y_te, x_ref, y_ref = prep_delay_data()
+    print(x_tr.shape, x_te.shape, y_tr.shape, y_te.shape)
---
+++ setup.py
@@ -0,0 +1,145 @@
+import sys
+import os
+
+import numpy as np
+
+
+def get_inputs(source, files):
+    # init empty placeholders
+    ebeam_labels = []
+    ebeam_data = []
+    epic_labels = []
+    epic_data = []
+    gmd_labels = []
+    gmd_data = []
+
+    for f in files:
+        if any([i in f for i in ["EBeam", "EPICS", "GMD"]]):  # avoid unnecessary data loading
+            data = np.load(os.path.join(source, f))
+        if "EBeam" in f:
+            if not ebeam_labels:
+                ebeam_data = data["EBeamValuesList"]
+                ebeam_labels = data["EBeamParameterNames"].tolist()
+            else:
+                ebeam_data = np.append(ebeam_data, data["EBeamValuesList"], axis=0)
+        elif "EPICS" in f:
+            if not epic_labels:
+                epic_data = data["EPICSValuesList"]
+                epic_labels = data["EPICSParameterNames"].tolist()
+            else:
+                epic_data = np.append(epic_data, data["EPICSValuesList"], axis=0)
+        elif "GMD" in f:
+            if not gmd_labels:
+                gmd_data = data["GMDValuesList"]
+                gmd_labels = data["GMDParameterNames"].tolist()
+            else:
+                gmd_data = np.append(gmd_data, data["GMDValuesList"], axis=0)
+
+    labels = ebeam_labels + epic_labels + gmd_labels
+    labels = [str(label, "utf-8") for label in labels]
+    temp_data = np.append(ebeam_data, epic_data, axis=1)
+    data = np.append(temp_data, gmd_data, axis=1)
+
+    return data, labels
+
+
+def format_single(source, target):
+
+    files = os.listdir(source)
+
+    inp, inp_labels = get_inputs(source, files)
+
+    # initialise output features
+    optical_data = []
+    optical_labels = []
+
+    for f in files:
+        if "Optical" in f:
+            data = np.load(os.path.join(source, f), allow_pickle=True, encoding="latin1")
+            sf_lam = lambda x: np.array([0, 0, 0]) if x is None else x
+            sf_list = [sf_lam(i) for i in data["UXSSingleFitList"]]
+            sf_data = np.stack(sf_list)
+            if not optical_labels:
+                optical_labels = ["GaussAmp", "GaussMean_eV", "GaussStd_eV", "FitMask"]\
+                                 + data["xUXS"].astype("str").tolist()
+
+                fit_info = np.append(sf_data, np.array([data["UXSSingleFitListMask"]]).T, axis=1)
+                optical_data = np.append(fit_info, data["UXSProfileList"], axis=1)
+            else:
+                fit_info = np.append(sf_data, np.array([data["UXSSingleFitListMask"]]).T, axis=1)
+                data = np.append(fit_info, data["UXSProfileList"], axis=1)
+                optical_data = np.append(optical_data, data, axis=0)
+
+    d = os.getcwd()
+    if not os.path.exists(target):
+        os.mkdir(target)
+    os.chdir(target)
+    np.savetxt("single_inputs.tsv.gz", inp, delimiter="\t", header="\t".join(inp_labels), comments="")
+    np.savetxt("single_outputs.tsv.gz", optical_data, delimiter="\t", header="\t".join(optical_labels), comments="")
+    os.chdir(d)
+
+
+def format_double(source, target):
+    files = os.listdir(source)
+
+    inp, inp_labels = get_inputs(source, files)
+
+    # initialise output features
+    delay_data = []
+    delay_labels = []
+    tof_data = []
+    tof_labels = []
+
+    for f in files:
+        if "Delay" in f:
+            data = np.load(os.path.join(source, f))
+            if not delay_labels:
+                delay_labels = ["Delays", "DelayMask"]
+                delay_data = np.array([data["DelayValuesList"].flatten(), data["DelayValuesListMask"]]).T
+            else:
+                temp_data = np.array([data["DelayValuesList"].flatten(), data["DelayValuesListMask"]]).T
+                delay_data = np.append(delay_data, temp_data, axis=0)
+
+        elif "TOF" in f:
+            data = np.load(os.path.join(source, f))
+            if not tof_labels:
+                tof_labels = ["LowGaussAmp", "LowGaussMean_eV", "LowGaussStd_eV", "HighGaussAmp", "HighGaussMean_eV",
+                              "HighGaussStd_eV", "DelaysMask"] + data["xTOF"].astype("str").tolist()
+
+                fit_info = np.append(data["TOFDoubleFitList"], np.array([data["TOFDoubleFitListMask"]]).T, axis=1)
+                tof_data = np.append(fit_info, data["TOFProfileList"], axis=1)
+
+            else:
+                fit_info = np.append(data["TOFDoubleFitList"], np.array([data["TOFDoubleFitListMask"]]).T, axis=1)
+                temp_data = np.append(fit_info, data["TOFProfileList"], axis=1)
+                tof_data = np.append(tof_data, temp_data, axis=0)
+
+    output = np.append(delay_data, tof_data, axis=1)
+    output_labels = delay_labels + tof_labels
+
+    d = os.getcwd()
+    if not os.path.exists(target):
+        os.mkdir(target)
+    os.chdir(target)
+    np.savetxt("double_inputs.tsv.gz", inp, delimiter="\t", header="\t".join(inp_labels), comments="")
+    np.savetxt("double_outputs.tsv.gz", output, delimiter="\t", header="\t".join(output_labels), comments="")
+    os.chdir(d)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        pass  # Implement raise error later
+    f_path = os.path.dirname(sys.argv[0])
+    target_dir = os.path.join(f_path, "Data")
+    if not os.path.exists(os.path.join(f_path, "DataLCLS2017")):
+        os.system("git clone https://github.com/alvarosg/DataLCLS2017.git")  # original data
+
+    data_dir = os.path.join(f_path, "DataLCLS2017/Data/")
+    single_dir = os.path.join(data_dir, "amof6215")
+    double_dir = os.path.join(data_dir, "amo86815")
+
+    os.chdir(f_path)
+    format_single(single_dir, target_dir)
+    format_double(double_dir, target_dir)
+
+
