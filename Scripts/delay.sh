a1a1be160c7af7e7fc83260cd33a4d07172a9e8c
Karim <karim.alaael-din@gmx.de>
2021-01-30 19:56:13 +0100

    fix: submission script adjusted to use cli

--- Scripts/delay.sh
+++ Scripts/delay.sh
@@ -4,6 +4,6 @@
 module load anaconda3/personal
 module load cuda
 
-python $HOME/XFEL-ANN/delay_est.py $HOME
+python $HOME/XFEL-ANN/model_fit.py Delays --shape 50 50 20 20 20 -vv -a relu -l mae -e 40000 --no-batch_norm
 mkdir $WORK/delay_$PBS_JOBID
 cp -r * $WORK/delay_$PBS_JOBID
