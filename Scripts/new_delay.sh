7533c65630680632568b262968e853abe40c6083
Karim <karim.alaael-din@gmx.de>
2021-03-01 20:31:11 +0000

    feat: added new Data + Scripts

---
+++ Scripts/new_delay.sh
@@ -0,0 +1,9 @@
+#PBS -lwalltime=10:00:00
+#PBS -lselect=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
+
+module load anaconda3/personal
+module load cuda
+
+python $HOME/XFEL-ANN/model_fit.py NewDelays --shape 50 50 20 20 20 -vv -a relu -l mae -e 40000 --batch_norm False -r 0.0015
+mkdir $WORK/delay_$PBS_JOBID
+cp -r * $WORK/delay_$PBS_JOBID
