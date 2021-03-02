04f79ffb7898a2b09c4baadc72653e437f91dc35
Karim <karim.alaael-din@gmx.de>
2021-03-02 16:03:49 +0000

    fix: readjusted settings

--- Scripts/emean.sh
+++ Scripts/emean.sh
@@ -4,6 +4,6 @@
 module load anaconda3/personal
 module load cuda
 
-python $HOME/XFEL-ANN/model_fit.py EMean --shape 10 10 10 10 10 10 -vv -a relu -l mae -e 20000 --batch_norm False -p 1000
+python $HOME/XFEL-ANN/model_fit.py EMean --shape 10 10 10 10 10 10 -vv -a relu -l mae -e 20000 --batch_norm False -p 1000 -reg l2
 mkdir $WORK/emean_$PBS_JOBID
 cp -r * $WORK/emean_$PBS_JOBID
--- Scripts/new_delay.sh
+++ Scripts/new_delay.sh
@@ -4,6 +4,6 @@
 module load anaconda3/personal
 module load cuda
 
-python $HOME/XFEL-ANN/model_fit.py NewDelays --shape 50 50 20 20 20 -vv -a relu -l mae -e 40000 --batch_norm False -r 0.0015
+python $HOME/XFEL-ANN/model_fit.py NewDelays --shape 20 20 20 20 20 -vv -a relu -l mae -e 40000 --batch_norm False -r 0.003 -reg l2 --drop_out 0.025
 mkdir $WORK/delay_$PBS_JOBID
 cp -r * $WORK/delay_$PBS_JOBID
